8f2edf2f46f0b3e45216b9ec7d28ef34041b8dcc
Pintu Kumar <149261931+intensity4143@users.noreply.github.com>
2025-07-17 01:31:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,12 @@ require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
 // middleware 
-app.use(cors());
+app.use(cors({
+  origin: "https://task-manager-rfo2.onrender.com", // your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
